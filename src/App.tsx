4df68b47f14caa7bafec2522fa6b4131e5d0205b
Nathaniel Richards <nthnlrichards@gmail.com>
2022-09-11 14:28:52 -0400

    Got tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Searchbox } from "./components/Searchbox";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl mt-12 font-bold">
+        👋🏻 Hey, I'm Nathaniel Richards
+      </h1>
+      <p className="mt-8">
+        This site is about all things code. I am a software engineer and
+        lifelong learner. The official use of this site is as a journal and
+        digital vault for all things me <span>(Nathaniel Richards)</span>, and
+        engineering projects / endeavors.
+      </p>
     </div>
   );
 }
